7955689ea0d5a40fe5c7be6c6b4aa3d76cb30d26
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-23 16:56:09 +0100

    + bonus

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // stampare a schermo un messaggio all'interno
 // di un h1 utillizando i data
 
+// Aggiungere alla pagina un’immagine,
+//  presa anch’essa da un data.
+
 var vue = new Vue(
   {
     el: '#root',
@@ -9,11 +12,6 @@ var vue = new Vue(
     messaggioInInput: '',
     message: 'Hello Mars!',
     mieClassi: 'blu',
+    image: './mars.jpeg' 
   }
-  // methods: {
-  //   saluta: function(){
-  //     return 'Ciao Ciao!'
-  //   }
-  // },
-
   });
