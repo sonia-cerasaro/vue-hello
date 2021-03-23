7e4fbf0328fb66707a85958d0a6b121429d6f94d
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-23 16:08:24 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,19 @@
+// stampare a schermo un messaggio all'interno
+// di un h1 utillizando i data
+
+var vue = new Vue(
+  {
+    el: '#root',
+    data: {
+    classeRed: 'saluto',
+    messaggioInInput: '',
+    message: 'Hello Mars!',
+    mieClassi: 'blu',
+  }
+  // methods: {
+  //   saluta: function(){
+  //     return 'Ciao Ciao!'
+  //   }
+  // },
+
+  });
